49093eb7a35d928f022d5b04d4e8eadf65727e85
infinityshades <tash.aneiq@gmail.com>
2020-02-20 20:46:04 +0100

    schema first commit

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(process.env.MONGODBURL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(db => console.log(`connected to mongoDB`), err => console.log(`Error connecting to mongoDB`, err))
+.then(db => console.log(`connected to mongoDB`), err => console.log(`Error connecting to mongoDB`, err));
 
-const port = process.env.PORT || 8181
+const port = process.env.PORT || 8181;
 
 app.listen(port,() =>{
-    console.log(`Server is launched at launchpad ${port}`)
-})
+    console.log(`Server is launched at launchpad ${port}`);
+});
+
+app.use(express.json());
---
+++ src/models/users/index.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const experienceSchema = new mongoose.Schema({
+    role: String,
+    company: String,
+    startDate: Date,
+    endDate: Date,
+    description: {
+        type: String,
+        maxlength:[500, 'Maximum 500 characters']
+    },
+    area: String,
+    images:{
+        type: String,
+        default: "https://designshack.net/wp-content/uploads/placeholder-image.png"
+    }
+}, {timestamps: true})
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    bio:{
+        type: String,
+        maxlength: [250, 'Maximum 500 characters']
+    }, 
+    title:String,
+    area: String,
+    image:{
+        type: String,
+        default: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder-300-grey.jpg"
+    },
+    experiences:[experienceSchema]
+}, { timestamps: true})
+
+const userCollection = mongoose.model('users', userSchema);
